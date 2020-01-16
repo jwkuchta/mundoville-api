d173f6efd9978a29ae96ad46821860b94e967005
Justina Kuchta <justyna.kuchta@gmail.com>
2020-01-15 19:13:11 -0500

    struggling with async actions and redux store

--- config/routes.rb
+++ config/routes.rb
@@ -9,3 +9,12 @@ Rails.application.routes.draw do
     end
   end
 end
+
+# post '/login', to: 'auth#create'
+# get '/profile', to: 'users#profile'
+# post '/find', to: 'follows#removeFollow'
+# post '/remove', to: 'close_friends#remove'
+# post '/getConvos', to: 'conversations#getConvos'
+# post '/upload', to: 'photos#upload'
+# post '/photos', to: 'photos#allphotos'
+# post '/featured', to: 'photos#featured'
