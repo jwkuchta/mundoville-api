3adddcbf599cd1d7f86c11d86f4e5458fbc5ff2b
Justina Kuchta <justyna.kuchta@gmail.com>
2020-01-23 10:27:38 -0500

    working version, no reviews added yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_20_145853) do
+ActiveRecord::Schema.define(version: 2020_01_23_045555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,11 @@ ActiveRecord::Schema.define(version: 2020_01_20_145853) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["exchange_id"], name: "index_messages_on_exchange_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
