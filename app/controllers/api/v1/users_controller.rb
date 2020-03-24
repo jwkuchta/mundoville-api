33d81d638943c1352611f9021324e04c4d7c54e0
Justina Kuchta <justyna.kuchta@gmail.com>
2020-03-23 21:30:41 -0400

    added id_provider for facebook and twitter log in

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -74,7 +74,7 @@ class API::V1::UsersController < ApplicationController
   def user_params
     # params.require(:user).permit(:id, :username, :first_name, :profile_pic, :last_name, :email, :password, :bio, :country, :city, :language1, :language2, :language3, :occupation, :yob)
     # params.require(:user).permit(:id, :updated_at, :email_verified, :sub, :nickname, :name, :first_name, :profile_pic, :picture, :last_name, :email, :password, :bio, :country, :city, :language1, :language2, :language3, :occupation, :yob)
-    params.require(:user).permit(:id, :first_name, :last_name, :email, :name, :nickname, :picture, :profile_pic, :sub, :occupation, :age, :language1, :language2, :language3, :bio, :country, :city, :yob, :updated_at)
+    params.require(:user).permit(:id, :id_provider, :first_name, :last_name, :email, :name, :nickname, :picture, :profile_pic, :sub, :occupation, :age, :language1, :language2, :language3, :bio, :country, :city, :yob, :updated_at)
   end
 
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_130534) do
+ActiveRecord::Schema.define(version: 2020_03_23_200355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2020_03_11_130534) do
     t.string "sub"
     t.string "name"
     t.string "picture"
+    t.string "id_provider"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
